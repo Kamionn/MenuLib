36e0377dafebbdf44e2667ebd5af0ddb11234af1
Matthéo Le Fur <mattheo.lefur41@gmail.com>
2025-05-19 11:22:13 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kamion'
+description 'MenuLib - Système de menu dynamique et modulaire pour FiveM'
+version '1.0.0'
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+ui_page 'web/build/index.html'
+
+files {
+    'web/build/index.html',
+    'web/build/**/*'
+}
